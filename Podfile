04c80fbf07854e10bb6ce136736f1bfd16cb2b91
Dennis <dennis.fedorko@gmail.com>
2016-10-26 13:57:00 -0400

    Update Pods to Swift 3

--- Podfile
+++ Podfile
@@ -3,5 +3,5 @@ use_frameworks!
 target ‘big-red-shuttle’ do
   pod 'GoogleMaps'
   pod 'GooglePlaces'
-  pod 'SwiftyJSON', '3.0.0'
+  pod 'SwiftyJSON'
 end
--- Pods/GoogleMaps/Example/GoogleMapsDemos/Samples/FitBoundsViewController.m
+++ Pods/GoogleMaps/Example/GoogleMapsDemos/Samples/FitBoundsViewController.m
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016 Google Inc. All rights reserved.
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+ * ANY KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
@@ -51,16 +66,14 @@
 }
 
 - (void)didTapFitBounds {
-  GMSCoordinateBounds *bounds;
+  if ([_markers count] == 0) return;
+  CLLocationCoordinate2D firstPos = ((GMSMarker *)_markers.firstObject).position;
+  GMSCoordinateBounds *bounds =
+      [[GMSCoordinateBounds alloc] initWithCoordinate:firstPos coordinate:firstPos];
   for (GMSMarker *marker in _markers) {
-    if (bounds == nil) {
-      bounds = [[GMSCoordinateBounds alloc] initWithCoordinate:marker.position
-                                                    coordinate:marker.position];
-    }
     bounds = [bounds includingCoordinate:marker.position];
   }
-  GMSCameraUpdate *update = [GMSCameraUpdate fitBounds:bounds
-                                           withPadding:50.0f];
+  GMSCameraUpdate *update = [GMSCameraUpdate fitBounds:bounds withPadding:50.0f];
   [_mapView moveCamera:update];
 }
 
