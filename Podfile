54221bb6b94d2d63d20cc6b94a0c3945668068fb
Dennis <dennis.fedorko@gmail.com>
2016-10-26 14:08:36 -0400

    Add Alamofire to project

--- Podfile
+++ Podfile
@@ -4,4 +4,13 @@ target ‘big-red-shuttle’ do
   pod 'GoogleMaps'
   pod 'GooglePlaces'
   pod 'SwiftyJSON'
+  pod 'Alamofire'
+end
+
+post_install do |installer|
+    installer.pods_project.targets.each do |target|
+        target.build_configurations.each do |config|
+            config.build_settings['SWIFT_VERSION'] = '3.0'
+        end
+    end
 end
--- Pods/Target Support Files/Pods-big-red-shuttle/Pods-big-red-shuttle-frameworks.sh
+++ Pods/Target Support Files/Pods-big-red-shuttle/Pods-big-red-shuttle-frameworks.sh
@@ -84,8 +84,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods-big-red-shuttle/Alamofire.framework"
   install_framework "Pods-big-red-shuttle/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods-big-red-shuttle/Alamofire.framework"
   install_framework "Pods-big-red-shuttle/SwiftyJSON.framework"
 fi
