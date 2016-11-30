0144c5490c5c47166ead20c99de1544da38afb6d
AAAstorga <astorgaaustin@gmail.com>
2016-11-30 15:49:42 -0500

    fixed PR issues

--- big-red-shuttle/Controllers/DriverViewController.swift
+++ big-red-shuttle/Controllers/DriverViewController.swift
@@ -8,17 +8,16 @@ class DriverViewController: UIViewController, UITableViewDelegate, UITableViewDa
     var isLoggingOn = false
     var textFieldText: String = ""
     var loginSuccessful: Bool = false
+    let masterKey = "brsxappdev2016"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        navigationController?.navigationBar.topItem?.title = "Settings"
+        title = "Settings"
         let closeButton = UIBarButtonItem(barButtonSystemItem: .stop, target: self, action: #selector(dismissView))
         closeButton.tintColor = .darkGray
         navigationController?.navigationBar.topItem?.rightBarButtonItem = closeButton
         view.backgroundColor = .brslightgrey
-        let tableViewFrame = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height)
-        tableView = UITableView(frame: tableViewFrame, style: .grouped)
+        tableView = UITableView(frame: view.bounds, style: .grouped)
         tableView.isScrollEnabled = false
         tableView.dataSource = self
         tableView.delegate = self
@@ -50,24 +49,21 @@ class DriverViewController: UIViewController, UITableViewDelegate, UITableViewDa
             cell.textLabel?.text = "Authentication Key"
 
             if loginImage != nil {
-            cell.imageView?.image = loginImage
-            textField.text = textFieldText
+                cell.imageView?.image = loginImage
+                textField.text = textFieldText
             }
-            textField.placeholder = "Insert Key"
-            textField.textAlignment = .right
-            textField.returnKeyType = .go
-            textField.isSecureTextEntry = true
-            textField.addTarget(self, action: #selector(textFieldDidChange), for: .editingDidEndOnExit)
-        cell.accessoryView = textField
+                textField.placeholder = "Insert Key"
+                textField.textAlignment = .right
+                textField.returnKeyType = .go
+                textField.isSecureTextEntry = true
+                textField.addTarget(self, action: #selector(textFieldDidChange), for: .editingDidEndOnExit)
+                cell.accessoryView = textField
         } else {
-            if loginSuccessful {
-                cell.isUserInteractionEnabled = true
-                cell.textLabel?.textColor = .black
-            } else {
-                cell.isUserInteractionEnabled = false
-                cell.textLabel?.textColor = .lightGray
-                cell.textLabel?.alpha = 0.3
-            }
+            cell.isUserInteractionEnabled = loginSuccessful
+            cell.textLabel?.textColor = loginSuccessful ? . black : .lightGray
+            cell.textLabel?.alpha = loginSuccessful ? 1.0 : 0.3
+            
+            
             cell.textLabel?.text = "Log Location"
             let logSwitch = UISwitch()
             logSwitch.isOn = isLoggingOn
@@ -85,43 +81,35 @@ class DriverViewController: UIViewController, UITableViewDelegate, UITableViewDa
     
     func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
         var footerString = ""
-        if loginSuccessful && isLoggingOn{
+        if loginSuccessful && isLoggingOn {
             footerString = "You are now logging the location of your phone to the application and everyone on the network. Please turn off logging while you are not on the bus."
         } else if loginSuccessful && !isLoggingOn {
             footerString = "You are currently not logging the location of the bus. Click the toggle to start logging."
         } else if !loginSuccessful {
             footerString = "Your Authentication Key is incorrect. You are currently not logging the location."
-            }
+        }
         
         return footerString
     }
     
     func textFieldDidChange(textField: UITextField) {
         //check if access key is correct if so, unlock cell aka brsxappdev2016
-        if textField.text == "brsxappdev2016" {
-            loginImage = UIImage(named: "checkmark")
-            textFieldText = "brsxappdev2016"
-            loginSuccessful = true
-            tableView.reloadData()
-        } else {
-            loginImage = UIImage(named: "incorrect")
-            textFieldText = textField.text!
-            loginSuccessful = false
-            tableView.reloadData()
-        }
+        loginImage = textField.text == masterKey ? UIImage(named: "checkmark") : UIImage(named: "incorrect")
+        textFieldText = textField.text == masterKey ? "brsxappdev2016" : textField.text!
+        loginSuccessful = textField.text == masterKey ? true : false
+        tableView.reloadData()
     }
     
     func didToggleLogging(loggingSwitch: UISwitch) {
-        if loggingSwitch.isOn {
-            isLoggingOn = true
+        isLoggingOn = loggingSwitch.isOn
+        if isLoggingOn {
             //MARK: start logging location
-            tableView.reloadData()
         } else {
-            isLoggingOn = false
             //MARK stop logging location
-            tableView.reloadData()
         }
+        tableView.reloadData()
     }
+    
     
 
 }
